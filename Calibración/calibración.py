c11a1c3019d9f685a1c35cf9248dccb6cabec241
Horax <horaciogarciacr@gmail.com>
2026-05-27 20:04:52 -0300

    Modelo de prueba de barrido

--- Calibración/calibración.py
+++ Calibración/calibración.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import pandas as pd 
 
 # 1. Configuración de rutas y parámetros
-trayectorias_path = r'Analisis de video\Datos_tray\Discreto_y_2605.csv'
+trayectorias_path = r'Analisis de video\Datos_tray\Discreto_y_2705_v2_proc.csv'
 data = pd.read_csv(trayectorias_path)
 filas = data['fila'].unique()
 
-x_1 = data[data['fila']==filas[0]]['X']
-y_1 = data[data['fila']==filas[0]]['Y']
+x_1 = data[data['fila']==filas[1]]['X']
+y_1 = data[data['fila']==filas[1]]['Y']
 
 # plt.scatter(x_1,y_1)
 # plt.xlabel('x [px]')
@@ -19,7 +19,6 @@ y_1 = data[data['fila']==filas[0]]['Y']
 
 def lineal(x,a,b):
     return a*x + b
-
 pendientes = []
 ordenadas = []
 
@@ -37,8 +36,13 @@ for fila in filas:
 pendiente_promedio = np.mean(pendientes)
 ordenada_promedio = np.mean(ordenadas)
 
+print(pendiente_promedio)
+print(ordenada_promedio)
+
+
+
 # plt.hist(pendientes)
-# plt.show()
+#plt.show()
 
 # Código para promediar los clusters obtenidos en el barrido discreto 
 
@@ -52,7 +56,7 @@ def promediar_clusters_corregido(x_data, y_data, umbral_x):
     
     # 1. Calculamos la derivada discreta frame a frame.
     # Le ponemos np.abs() para que detecte saltos tanto en la ida como en la vuelta del barrido.
-    dx_frame = np.abs(np.diff(x))
+    dx_frame = np.abs(np.diff(y))
     
     # 2. El salto físico real de un duty cycle a otro será mucho mayor que 
     # el ruido de trackeo entre dos frames del mismo clúster estacionario.
@@ -73,65 +77,99 @@ def promediar_clusters_corregido(x_data, y_data, umbral_x):
     y_err = np.array([np.std(c) for c in clusters_y if len(c) > min_frames_por_paso])
     
     # Imprimimos diagnóstico rápido para que veas qué hizo
-    print(f"Detectados {len(x_promedios)} clústers (pasos) válidos.")
+    print(f"Detectados {len(y_promedios)} clústers (pasos) válidos.")
     
     return x_promedios, y_promedios, x_err, y_err
 
-# --- Ejemplo de uso ---
-# Suponiendo que ya cargaste tus datos de tracking del video (ej. con pd.read_csv o np.loadtxt)
-x_track = data[data['fila']==filas[5]]['X']
-y_track = data[data['fila']==filas[5]]['Y']
+ajustes_nolineal_y = []
 
-umbral_x = 1  # Ajustá esto según el "paso" en micrómetros o píxeles de tu barrido
+for i,fila in enumerate(filas):
 
-x_mean, y_mean, x_std, y_std = promediar_clusters_corregido(x_track, y_track, umbral_x)
+    if i not in [6]:
+        x_track = data[data['fila']==fila]['X']
+        y_track = data[data['fila']==fila]['Y']
 
-print(len(x_mean))
+        umbral_x = 1  # Ajustá esto según el "paso" en micrómetros o píxeles de tu barrido
 
-# # Visualización rápida para ver que haya quedado joya
-plt.figure(figsize=(8, 6))
-plt.scatter(x_track, y_track, color='gray', alpha=0.3, label='Tracking crudo')
-plt.errorbar(x_mean, y_mean, xerr=x_std, yerr=y_std, fmt='ro', 
-             capsize=3, label='Centroides (promedio)')
-plt.xlabel('Desplazamiento X')
-plt.ylabel('Desplazamiento Y')
-plt.title('Promediado de Clústers de Tracking')
-plt.legend()
-plt.grid(True)
+        x_mean, y_mean, x_std, y_std = promediar_clusters_corregido(x_track, y_track, umbral_x)
+
+        print(len(x_mean))
+
+        # Visualización rápida para ver que haya quedado joya
+        # plt.figure(figsize=(8, 6))
+        # plt.scatter(x_track, y_track, color='gray', alpha=0.3, label='Tracking crudo')
+        # plt.errorbar(x_mean, y_mean, xerr=x_std, yerr=y_std, fmt='ro', 
+        #             capsize=3, label='Centroides (promedio)')
+        # plt.xlabel('Desplazamiento X')
+        # plt.ylabel('Desplazamiento Y')
+        # plt.title(f'Promediado de Clústers de Tracking {i}')
+        # plt.legend()
+        # plt.grid(True)
+        # plt.show()
+
+        dc_x = range(0,65535,3276)
+        dc_x = (1/65535)*np.array(dc_x)
+
+        print(dc_x)
+        # Calibración de distancia
+        y_mean = y_mean*0.025239
+
+        coef = np.polyfit(dc_x,y_mean,3)
+        ajustes_nolineal_y.append((i,coef))
+        def pol4(x,c,d,e,f):
+            return  c*x**3 + d*x**2 + e*x + f
+
+        plt.plot(dc_x,pol4(dc_x,*coef),'r',label='Ajuste polinomio grado 4')
+        plt.scatter(dc_x,y_mean)
+        plt.title('Histéresis de ida en x')
+        plt.xlabel('Duty cycle')
+        plt.ylabel('x[um]')
+        plt.gca().invert_yaxis()
+        plt.grid(True)
+
+print(ajustes_nolineal_y)
+
 plt.show()
 
-dc_x = range(3276,65535,3276)
-dc_x = (1/65535)*np.array(dc_x)
 
-# Calibración de distancia
-x_mean = x_mean*0.025239
+coef_principales = [coefs[0] for fila,coefs in ajustes_nolineal_y]
+print(coef_principales)
 
-# coef = np.polyfit(dc_x[0:18],x_mean,4)
-# def pol4(x,a,b,c,d,e):
-#     return a*x**4 + b*x**3 + c*x**2 + d*x + e
+# Juntamos el número con los elementos del array en una sola lista por fila
+filas = [[numero] + list(array) for numero, array in ajustes_nolineal_y]
 
-# plt.plot(dc_x[0:18],pol4(dc_x[0:18],*coef),'r',label='Ajuste polinomio grado 4')
-# plt.scatter(dc_x[0:18],x_mean)
-# plt.title('Histéresis de ida en x')
-# plt.xlabel('Duty cycle')
-# plt.ylabel('x[um]')
+# Creamos el DataFrame
+df = pd.DataFrame(filas)
+
+# (Opcional) Si querés ponerle nombres a las columnas para que quede prolijo:
+df.columns = ['Fila', 'a', 'b', 'c','d']
+
+# Lo exportamos a .csv sin el índice automático de pandas
+df.to_csv('ajuste_cubico_y.csv', index=False)
+# x_track = data[data['fila']==0]['X']
+# y_track = data[data['fila']==0]['Y']
+
+# umbral_x = 1  # Ajustá esto según el "paso" en micrómetros o píxeles de tu barrido
+
+# x_mean, y_mean, x_std, y_std = promediar_clusters_corregido(x_track, y_track, umbral_x)
+
+# print(coef)
+
+# plt.scatter(range(0,len(x_mean)),x_mean)
+# plt.title('Saltos en posición en función del dc')
+# plt.xlabel('Duty cycle
+# plt.ylabel('\Delta x[um]')  
 # plt.gca().invert_yaxis()
 # plt.grid(True)
 # plt.show()
 
 
-x_track = data[data['fila']==0]['X']
-y_track = data[data['fila']==0]['Y']
+pend_promedio_cross_x = -0.03461811951503092
+ordenada_promedio_cross_x = 316.7424179043115
 
-umbral_x = 1  # Ajustá esto según el "paso" en micrómetros o píxeles de tu barrido
+pend_promedio_cross_y = -0.07531236878047691
+ordenada_promedio_cross_y = 1485.4739436704124
 
-x_mean, y_mean, x_std, y_std = promediar_clusters_corregido(x_track, y_track, umbral_x)
 
-plt.scatter(range(0,len(x_mean)),x_mean)
-plt.title('Saltos en posición en función del dc')
-plt.xlabel('Duty cycle')
-plt.ylabel('\Delta x[um]')  
-plt.gca().invert_yaxis()
-plt.grid(True)
-plt.show()
+
 
---
+++ Newton_Raphson_funcional.py
@@ -0,0 +1,86 @@
+"""
+@author: Horax & Fedex
+"""
+
+import pandas as pd 
+import numpy as np
+
+
+promedio_cross_x = [-0.03461811951503092,0]
+promedio_cross_y = [-0.07531236878047691,0]
+
+datos_ajuste_x = pd.read_csv('ajuste_cubico_x.csv') 
+datos_ajuste_y= pd.read_csv('ajuste_cubico_y.csv')
+
+promedio_x = datos_ajuste_x.mean()[1::]
+coefs_x = [coef for coef in promedio_x] 
+coefs_x[-1] = 0
+
+
+promedio_y = datos_ajuste_y.mean()[1::]
+coefs_y = [coef for coef in promedio_y] 
+coefs_y[-1] = 0
+
+
+cross_x = np.poly1d(promedio_cross_x)
+cross_y = np.poly1d(promedio_cross_x)
+
+
+polinomio_x = np.poly1d(coefs_x)
+polinomio_y = np.poly1d(coefs_y)
+
+d_polinomio_x = np.polyder(polinomio_x)
+d_polinomio_y = np.polyder(polinomio_y)
+
+
+
+# Newton-Raphson para un paso
+
+def NR_1paso_x(dc_x0,X):
+    F_x = polinomio_x(dc_x0) - X
+    return dc_x0 - F_x/d_polinomio_x(dc_x0)
+
+
+def NR_1paso_y(dc_y0,X,Y):
+    F_y = polinomio_y(dc_y0) + cross_x(X)- Y
+    return dc_y0 - F_y/d_polinomio_y(dc_y0)
+
+# Newton-Raphson en n iteraciones
+
+def NR_npasos_x(dc_x0,X,n):
+    dc_x = NR_1paso_x(dc_x0,X)
+    for _ in range(n-1):
+         dc_x = NR_1paso_x(dc_x,X)
+    return dc_x
+
+def NR_npasos_y(dc_y0,X,Y,n):
+    dc_y = NR_1paso_y(dc_y0,X,Y)
+    for _ in range(n-1):
+         dc_y = NR_1paso_y(dc_y,X,Y)
+    return dc_y
+
+def dc_scanning(paso_x,paso_y):
+
+    max_duty = 1
+
+    cant_pasos_x = int(max_duty/paso_x)
+    cant_pasos_y = int(max_duty/paso_y)
+
+    dc_x_inicial = np.linspace(0,max_duty,cant_pasos_x)
+    dc_y_inicial = np.linspace(0,max_duty,cant_pasos_y)
+
+    X_final = np.linspace(0,polinomio_x(1),cant_pasos_x)
+    Y_final = np.linspace(0,polinomio_y(1),cant_pasos_y)
+    dcs_x = []
+    dcs_y = []
+
+    for dc_x0,x in zip(dc_x_inicial,X_final):
+        dcx = NR_npasos_x(dc_x0,x,20)
+        dcs_x.append(dcx)
+        
+        for dc_y, y in zip(dc_y_inicial,Y_final):
+            dcy = NR_npasos_y(dc_y,x,y,20)
+            dcs_y.append(dcy)
+
+    return dcs_x, dcs_y
+
